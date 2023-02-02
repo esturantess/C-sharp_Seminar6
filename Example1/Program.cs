6c4d7d571fd2e1957dd98d4c9ffb5e89a850e814
Polina <ms.maylin@inbox.ru>
2023-02-02 22:14:42 +0300

    example1 added

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -4,3 +4,36 @@
 // [1 2 3 4 5] -> [5 4 3 2 1]
 // [6 7 3 6] -> [6 3 7 6]
 
+int[] FlipArray(int[] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        temp = array[i];
+        array[i] = array[array.Length - i - 1];
+        array[array.Length - i - 1] = temp;
+    }
+    return array;
+}
+
+void FillArray(int[] array)
+{
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(1, 11);
+    }
+}
+
+Random rand = new Random();
+int size = rand.Next(1, 25);
+int[] array = new int[size];
+
+FillArray(array);
+Console.WriteLine("[" + String.Join(", ", array) + "]");
+FlipArray(array);
+Console.WriteLine();
+Console.WriteLine("[" + String.Join(", ", array) + "]");
+
+
+
