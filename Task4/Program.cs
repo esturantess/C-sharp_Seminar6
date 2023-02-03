fedadf352eadf97a716068f6aacc7208a173c612
Polina <ms.maylin@inbox.ru>
2023-02-03 17:18:20 +0300

    In task4: another solution added in program.cs

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -38,3 +38,32 @@ int[] newArray = CopyArray(array);
 Console.WriteLine();
 Console.WriteLine("Копия массива: ");
 PrintArray(newArray);
+
+// Второй вариант решения:
+
+// int[] RandomArrayCreation(int size, int leftValue, int rightValue)
+// {
+//     int[] arr = new int[size];
+//     Random rand = new Random();
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = rand.Next(leftValue, rightValue + 1);
+//     }
+//     return arr;
+// }
+
+// int[] ArrayCopying(int[] array)
+// {
+//     int[] arrayCopy = new int[array.Length];
+//     for (int i = 0; i < arrayCopy.Length; i++)
+//     {
+//         arrayCopy[i] = array[i];
+//     }
+//     return arrayCopy;
+// }
+
+
+// int[] arr = RandomArrayCreation(5, 0, 10);
+// Console.WriteLine("[" + string.Join(",", arr) + "]");
+// int[] arr2 = ArrayCopying(arr);
+// Console.WriteLine("[" + string.Join(",", arr2) + "]");
